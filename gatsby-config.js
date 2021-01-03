c84784d97a5cd88c33b9be88e6896b0fcd4ffc3c
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2021-01-03 22:35:56 +0100

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-postcss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Alessio Bianchetti`,
+        // short_name: `abianche`,
+        start_url: `/`,
+        background_color: `#212121`,
+        theme_color: `#212121`,
+        icon: `src/images/icon.png`,
+        display: `browser`,
+      },
+    },
     `gatsby-plugin-offline`,
   ],
 }
