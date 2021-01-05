1bd825b1954393e5f5f6291e0a9449212da75508
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2021-01-05 17:18:49 +0100

    add tailwindcss ref

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -16,21 +16,38 @@ export default function Footer() {
 
   return (
     <footer className="p-4 px-6">
-      <div className="flex flex-col items-center">
-        <div className="text-center text-white">
-          Made with <i className="fas fa-bolt text-yellow-200"></i> using{" "}
+      <div className="flex flex-col items-center text-center text-gray-500">
+        <div>
+          Made with{" "}
+          <i
+            className="fas fa-bolt text-yellow-200 mx-1"
+            title="Lightning speed"
+          ></i>{" "}
+          using
           <a
             aria-label="gatsby"
             rel="noreferrer"
             target="_blank"
             href="https://www.gatsbyjs.com/"
+            className="mx-1 underline"
           >
             Gatsby
           </a>
+          and
+          <a
+            aria-label="tailwindcss"
+            rel="noreferrer"
+            target="_blank"
+            href="https://tailwindcss.com/"
+            className="mx-1 underline"
+          >
+            Tailwind CSS
+          </a>
         </div>
-        <div className="text-center text-white">
+        <div>
           © {new Date().getFullYear()} {data.site.siteMetadata.title}
         </div>
+        <div>All rights reserved</div>
       </div>
     </footer>
   )
