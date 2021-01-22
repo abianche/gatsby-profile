1f35e889e3cab3acac3bf2e2f3c6e0a14066c585
Alessio Bianchetti <alessio.bianchetti@gmail.com>
2021-01-22 23:50:15 +0100

    add hover effect

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -17,7 +17,10 @@ export default function Header({ siteTitle, pageInfo }) {
   return (
     <header>
       <nav className="flex justify-center p-4">
-        <Link className="text-xl text-white font-semibold font-heading" to="/">
+        <Link
+          className="text-xl text-white hover:text-yellow-200 font-semibold font-heading"
+          to="/"
+        >
           {data.site.siteMetadata.title.toUpperCase()}
         </Link>
       </nav>
